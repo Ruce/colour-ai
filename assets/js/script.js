e8902d04637aa3d665b0e3261766759e76f9bbf0
Ruce <bruce.togo@gmail.com>
2023-07-31 13:29:57 +0100

    Add interface elements and styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,14 @@ function inputReceived(textboxElement) {
 	const inputText = textboxElement.value;
 	textboxElement.value = "";
 	setRandomBoxColours();
+	
+	resetBoxHighlights();
+	highlightBox();
+}
+
+function nextClicked() {
+	setRandomBoxColours();
+	resetBoxHighlights();
 }
 
 function generateRandomColour() {
@@ -26,7 +34,6 @@ function setRandomBoxColours() {
 	changeBoxColour(document.getElementById("box-0"), randomColour0);
 	changeBoxColour(document.getElementById("box-1"), randomColour1);
 	changeBoxColour(document.getElementById("box-2"), randomColour2);
-	console.log(randomColour0, randomColour1, randomColour2);
 }
 
 function getHslCssString(h, s, l) {
@@ -36,6 +43,24 @@ function getHslCssString(h, s, l) {
 function changeBoxColour(boxElement, colour) {
 	const colourString = getHslCssString(colour[0], colour[1], colour[2]);
 	boxElement.style.setProperty('background-color', colourString);
+}
+
+function resetBoxHighlights() {
+	document.getElementById('box-0-outer').classList.remove('show-border');
+	document.getElementById('box-1-outer').classList.remove('show-border');
+	document.getElementById('box-2-outer').classList.remove('show-border');
+	
+	document.getElementById('box-0-outer').style.setProperty('border-color', 'transparent');
+	document.getElementById('box-1-outer').style.setProperty('border-color', 'transparent');
+	document.getElementById('box-2-outer').style.setProperty('border-color', 'transparent');
+}
+
+function highlightBox() {
+	let randBox = Math.floor(Math.random() * 3);
+	let boxId = `box-${randBox}-outer`;
+	setTimeout(() => {
+		document.getElementById(boxId).classList.add('show-border');
+	}, 50);
 }
 
 
